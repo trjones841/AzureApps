d2fb7559e4bf502e553dee873c2f58f1311c7d58
Unknown <terry.jones@war-eagle.me>
2020-05-12 13:09:21 -0700

    Tested/verified bash script to automate build/setup for mydomian

--- autoscaledemo.sh
+++ autoscaledemo.sh
@@ -1,47 +1,46 @@
-#! /bin/bash
+#!/bin/bash
 #
 # Script as required to add/update the application running on apache2
 # in the Azure vmss.
 #
-echo "Citrix Auto-Scale Demo - host $HOSTNAME !"
+echo "Citrix Auto-Scale Demo - host $HOSTNAME"
 sudo apt update
 sudo apt install -y apache2
 
+# Create, then update index.html for mydoman
+sudo mkdir /var/www/mydomain
+sudo chown -R $USER:$USER /var/www/mydomain
+sudo chmod -R 755 /var/www/mydomain
+echo '<html><head><title>My Domain</title></head><body><H1>Welcome to My Domain!</H1></body></html>' | tee /var/www/mydomain/index.html
+
+# /etc/apache2/sites-available/mydomain.conf
+echo '<VirtualHost *:80>' | sudo tee -a /etc/apache2/sites-available/mydomain.conf
+echo '  ServerAdmin webmaster@localhost' | sudo tee -a /etc/apache2/sites-available/mydomain.conf
+echo '  ServerName mydomain' | sudo tee -a /etc/apache2/sites-available/mydomain.conf
+echo '  ServerAlias mydomain' | sudo tee -a /etc/apache2/sites-available/mydomain.conf
+echo '  DocumentRoot /var/www/mydomain' | sudo tee -a /etc/apache2/sites-available/mydomain.conf
+echo '  ErrorLog ${APACHE_LOG_DIR}/error.log' | sudo tee -a /etc/apache2/sites-available/mydomain.conf
+echo '  CustomLog ${APACHE_LOG_DIR}/access.log combined' | sudo tee -a /etc/apache2/sites-available/mydomain.conf
+echo '</VirtualHost>' | sudo tee -a  /etc/apache2/sites-available/mydomain.conf
 #
-# sudo mkdir /var/www/my_domain
-# sudo chown -R $USER:$USER /var/www/my_domain
-# sudo chmod -R 755 /var/www/my_domain
-# echo '<html><head><title>My Domain</title></head><body><H1>Welcome to My Domain!</H1></body></html>' | tee /var/www/my_domain/index.html
-# sudo vim /etc/apache2/sites-available/my_domain.conf
-#
-# <VirtualHost *:80>
-#    ServerAdmin webmaster@localhost
-#    ServerName my_domain
-#    ServerAlias my_domain
-#    DocumentRoot /var/www/your_domain
-#    ErrorLog ${APACHE_LOG_DIR}/error.log
-#    CustomLog ${APACHE_LOG_DIR}/access.log combined
-# </VirtualHost>
-#
-# sudo a2ensite your_domain.conf
-# sudo a2dissite 000-default.conf
-# sudo apache2ctl configtest
-# sudo systemctl restart apache2
+sudo a2ensite /etc/apache2/sites-available/mydomain_ssl.conf
+sudo a2dissite /etc/apache2/sites-available/000-default.conf
+sudo apache2ctl configtest
+sudo systemctl restart apache2
 #
 ## Enable SSL website
 #
+# /etc/apache2/sites-available/mydomain_ssl.conf
+echo '<VirtualHost *:443>' | sudo tee -a /etc/apache2/sites-available/mydomain_ssl.conf
+echo '  DocumentRoot /var/www/mydomain' | sudo tee -a /etc/apache2/sites-available/mydomain_ssl.conf
+echo '  SSLEngine on' | sudo tee -a /etc/apache2/sites-available/mydomain_ssl.conf
+echo '  SSLCertificateFile /etc/ssl/certs/ssl-cert-snakeoil.pem' | sudo tee -a /etc/apache2/sites-available/mydomain_ssl.conf
+echo '  SSLCertificateKeyFile /etc/ssl/private/ssl-cert-snakeoil.key' | sudo tee -a /etc/apache2/sites-available/mydomain_ssl.conf
+echo '  ErrorLog ${APACHE_LOG_DIR}/error.log' | sudo tee -a /etc/apache2/sites-available/mydomain_ssl.conf
+echo '  CustomLog ${APACHE_LOG_DIR}/access.log combined' | sudo tee -a /etc/apache2/sites-available/mydomain_ssl.conf
+echo '</VirtualHost>' | sudo tee -a /etc/apache2/sites-available/mydomain_ssl.conf
 #
-# sudo vim /etc/apache2/sites-available/my_ssl_domain.conf
-#
-# <VirtualHost 192.168.0.1:443>
-# 	DocumentRoot /var/www/
-#	SSLEngine on
-#	SSLCertificateFile /path/to/your_domain_name.crt
-#	SSLCertificateKeyFile /path/to/your_private.key
-#	SSLCertificateChainFile /path/to/DigiCertCA.crt
-# </VirtualHost>
-#
-# sudo a2enmod ssl
-# sudo a2ensite my_ssl_domain.conf
-# sudo systemctl restart apache2
+sudo a2enmod ssl
+sudo a2ensite mydomain_ssl.conf
+sudo systemctl restart apache2
 #
